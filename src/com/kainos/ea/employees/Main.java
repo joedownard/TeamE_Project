397dd93e9e8c0154bb476d8bc603500ea2002882
Conor Martin <conor.martin@kainos.com>
2021-06-17 12:17:50 +0100

    Simple login system

--- src/com/kainos/ea/employees/Main.java
+++ src/com/kainos/ea/employees/Main.java
@@ -1,8 +1,22 @@
 package com.kainos.ea.employees;
 
+import java.util.Scanner;
+import java.io.Console;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Welcome to the system");
+        System.out.println("---------------------\n\nPlease enter your username");
+
+        Scanner login = new Scanner(System.in);
+        String userName = login.nextLine();
+
+        System.out.println("Please enter your password");
+        String password = login.nextLine();
+
+        System.out.println(userName + " " + password);
+
     }
 }
